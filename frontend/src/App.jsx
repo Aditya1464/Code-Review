5dafd0ef8ee906c646cd99625c0d98620ed2d826
Aditya Singh <124818450+Aditya1464@users.noreply.github.com>
2025-05-20 10:16:45 +0530

    Update App.jsx
    render added for host

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   async function reviewCode() {
     setLoading(true); // Set loading to true when the review starts
     try {
-      const response = await axios.post("http://localhost:3000/ai/get-review", { code });
+      const response = await axios.post("https://code-review-20h7.onrender.com/ai/get-review", { code });
       setRev(response.data);
     } catch (error) {
       console.error(error);
